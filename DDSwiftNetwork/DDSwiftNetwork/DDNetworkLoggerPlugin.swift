eb1514d00fa26076a64ec9e4b933a2c9301a85c6
Henry <lhappledev@163.com>
2019-12-12 01:23:05 +0800

    [add]打印response中error的情况

--- DDSwiftNetwork/DDSwiftNetwork/DDNetworkLoggerPlugin.swift
+++ DDSwiftNetwork/DDSwiftNetwork/DDNetworkLoggerPlugin.swift
@@ -15,21 +15,11 @@ public final class DDNetworkLoggerPlugin: PluginType {
     /// Request-打印
     /// - Parameter target: <#target description#>
     public func willSend(_ request: RequestType, target: TargetType) {
+        if isWhiteList(target) {return}
+        
         var header : String = "[:]"
         if let headers = target.headers {
             header = headers.reduce("[", {"\($0)'\($1.key)'='\($1.value)', "}) + "]"
-        }
-        
-        var parm : String
-        switch target.task {
-        case .requestPlain:
-            parm = target.path
-        case .requestParameters(parameters: let dic, encoding: _):
-            parm = dic.reduce("[", {"\($0)'\($1.key)'='\($1.value)', "}) + "]"
-        case .requestCompositeData(bodyData: let data, urlParameters: let dic):
-            parm = dic.reduce("[", {"\($0)'\($1.key)'='\($1.value)', "}) + "]" + "\nbody: \(String(data: data, encoding: .utf8) ?? "(invalid request)")"
-        default:
-            parm = "\(target.task)"
         }
         
         let strResult =
@@ -38,7 +28,7 @@ public final class DDNetworkLoggerPlugin: PluginType {
         Method: \(target.method)
         URL: \(target.baseURL)\(target.path)
         Header: \(header)
-        Parms: \(parm)]
+        Parms: \(requestParm(target))]
         """
         
         #if DEBUG
@@ -53,13 +43,21 @@ public final class DDNetworkLoggerPlugin: PluginType {
     /// - Parameter result: <#result description#>
     /// - Parameter target: <#target description#>
     public func didReceive(_ result: Result<Response, MoyaError>, target: TargetType) {
-        var responseResult : Any
-        if case .success(let responses) = result,
-            let str = try? JSONSerialization.jsonObject(with: responses.data, options: .mutableContainers){
-            responseResult = str
+        if isWhiteList(target) {return}
+        
+        var responseResult : Any = ""
+        if case .success(let responses) = result {
+            if let str = try? JSONSerialization.jsonObject(with: responses.data, options: .mutableContainers){
+                responseResult = str
+            }else{
+                responseResult = "Received error/empty response data for\n \(target.baseURL)\(target.path)"
+            }
+        }else if case .failure(let error) = result{
+            responseResult = "Received error network response for\n \(target.baseURL)\(target.path)\nErrorCode:\(error.errorCode)\nRequest:\(requestParm(target))"
         }else{
-            responseResult = "Received empty network response for \(target.baseURL)\(target.path)"
+            responseResult = "Response UnKnow state"
         }
+        
         let strResult =
         """
         [NetworkLogger:Response
@@ -72,4 +70,23 @@ public final class DDNetworkLoggerPlugin: PluginType {
         
         NotificationCenter.default.post(name: DDSWIFTNETWORK_RESPONSE_LOGGER_KEY, object: strResult)
     }
+    
+    /// 请求参数格式化
+    /// - Parameter target: <#target description#>
+    private func requestParm(_ target:TargetType) -> String {
+        var parm : String
+        switch target.task {
+        case .requestPlain:
+            parm = target.path
+        case .requestParameters(parameters: let dic, encoding: _):
+            parm = dic.reduce("[", {"\($0)'\($1.key)'='\($1.value)', "}) + "]"
+        case .requestCompositeData(bodyData: let data, urlParameters: let dic):
+            parm = dic.reduce("[", {"\($0)'\($1.key)'='\($1.value)', "}) + "]" + "\nbody: \(String(data: data, encoding: .utf8) ?? "(invalid request)")"
+        default:
+            parm = "\(target.task)"
+        }
+        return parm
+    }
+    
+    
 }
---
+++ DDSwiftNetwork/DDSwiftNetwork/tool/CommonMethod.swift
@@ -0,0 +1,19 @@
+//
+//  CommonMethod.swift
+//  DDSwiftNetwork
+//
+//  Created by Henry on 2019/12/12.
+//  Copyright © 2019 刘恒. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+/// 白名单中是否包含该targetType
+/// - Parameter target: <#target description#>
+public func isWhiteList(_ target:TargetType) -> Bool {
+    guard let _ = target.whiteList.firstIndex(of: target.path) else {
+        return false
+    }
+    return true
+}
